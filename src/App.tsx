9ffbba33f917781dfb1f0cc22e5300787ee3ed57
LubHR <liubomyrhruzd89@gmail.com>
2024-05-07 18:01:11 +0300

    commit newbranch increment,decrement,reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import './App.css';
 
+interface AppState {
+    value: number
+}
+
 const App = () => {
+    const [counter, setCounter] = React.useState<AppState>({value: 0});
+
+    const increment = () => {
+        setCounter(prevState => ({value: prevState.value + 1}));
+    }
+
+    const decrement = () => {
+        setCounter(prevState => ({value: prevState.value - 1}))
+    }
+
+    const reset = () =>{
+        setCounter({value: 0});
+    }
+
     return (
         <div>
-
+            <h2>{counter.value}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>reset</button>
         </div>
     );
 };
